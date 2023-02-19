2e016930788c7e360b5c3852959ce61c8ee29c59
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-02-19 09:41:19 +0000

    Edited controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,24 @@
-class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-      end
-    
-      def new
-        @posts = Post.new
-      end
-    
-      def show
-        @place = Place.find_by({ "id" => params["id"] })
-      end
-    
-      def create
-        @posts = Post.new
-        @posts["title"] = params["posts"]["title"]
-        @posts["body"] = params["posts"]["body"]
-        @posts["date"] = params["posts"]["date"]
-        @posts.save
-        redirect_to "/posts"
-      end
-end
+class ContactsController < ApplicationController
+
+    def show
+      @post = Posts.find_by({ "id" => params["id"] })
+      @place = Place.find_by({ "id" => @contact["place_id"] })
+    end
+  
+    def new
+      @post = Post.new
+      @place = Place.find_by({ "id" => params["place_id"] })
+      @post["place_id"] = @place["id"]
+    end
+  
+    def create
+      @post = Post.new
+      @post["title"] = params["contact"]["title"]
+      @post["body"] = params["contact"]["body"]
+      @post["date"] = params["contact"]["date"]
+      @post.save
+      redirect_to "/places/#{@post["place_id"]}"
+    end
+  
+  end
+  
