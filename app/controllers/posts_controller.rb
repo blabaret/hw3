e62c8b0946146d41f54b034b08dc3a9722e0a584
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-02-19 07:33:34 +0000

    Place entry working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+      end
+    
+      def new
+        @posts = Post.new
+      end
+    
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+      end
+    
+      def create
+        @posts = Post.new
+        @posts["title"] = params["posts"]["title"]
+        @posts["body"] = params["posts"]["body"]
+        @posts["date"] = params["posts"]["date"]
+        @posts.save
+        redirect_to "/posts"
+      end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,24 +1,6 @@
 class Place < ApplicationRecord
   # talks to the "places" table
 
-  def index
-    @places = Place.all
-  end
-  
-  def show
-    @places = Place.find_by({ "id" => params["id"] })
-    posts = Post.where({"place_id" => @places["id"]})
-  end
-  
-  def new
-    @place = Place.new
-  end
-  
-  def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
-    redirect_to "/places"
-  end
+
 
 end
