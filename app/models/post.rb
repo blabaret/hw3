75411dcf37155eac2ee08f3f1ff5fe3a83b7220b
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-02-19 01:22:57 +0000

    Added views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,24 @@
 class Post < ApplicationRecord
   # talks to the "posts" table
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @posts = Post.new
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+  end
+
+  def create
+    @posts = Post.new
+    @posts["title"] = params["posts"]["title"]
+    @posts["body"] = params["posts"]["body"]
+    @posts["date"] = params["posts"]["date"]
+    @posts.save
+    redirect_to "/posts"
+  end
+
 end
